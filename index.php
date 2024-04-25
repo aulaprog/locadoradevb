6aec5111d85a4f7e520dac9bbf0547eed13d9a90
Agatha Christie <agathacsfusco@gmail.com>
2024-04-25 11:55:06 -0300

    Primeira aula finalizada

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOCADORA DEV</title>
+    <link rel="stylesheet" href="./css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>LOCADORA DEV</h1>
+       <ul>
+        <li>
+            <a href="index.php?meu=home">Home</a>
+        </li>
+        <li>
+            <a href="index.php?meu=lista-videos">Vídeos</a>
+        </li>
+        <li>
+            <a href="index.php?meu=lista-categorias">Categorias</a>
+        </li>
+        <li>
+            <a href="index.php?meu=lista-clientes">Clientes</a>
+        </li>
+        <li>
+            <a href="index.php?meu=locacoes">Locações</a>
+        </li>
+       </ul>
+    </header>
+</body>
+</html>
